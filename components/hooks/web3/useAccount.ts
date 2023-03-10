eb05fdb680ba36e4ef0d34dfbc66e92e081a7b09
Naglfar28 <aozora00321@gmail.com>
2023-03-10 10:36:38 +0800

    account

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,16 +1,58 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
+import { useEffect } from "react";
 
-type AccountHookFactory = CryptoHookFactory<string, string>;
+type UseAccountResponse = {
+  connect: () => void;
+};
+
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>;
 
 export type UseAccountHook = ReturnType<AccountHookFactory>;
-export const hookFactory: AccountHookFactory = (deps) => (params) => {
-  const swrRes = useSWR("web3/useAccount", () => {
-    console.log(deps);
-    console.log(params);
-    // making request to get data
-    return "Test User";
-  });
+export const hookFactory: AccountHookFactory =
+  ({ provider, ethereum }) =>
+  () => {
+    const swrRes = useSWR(
+      provider ? "web3/useAccount" : null,
+      async () => {
+        const accounts = await provider!.listAccounts();
+        const account = accounts[0];
+        console.log(account);
+        if (!account) {
+          throw "cannot connect to the web3 wallet";
+        }
+        return accounts;
+      },
+      {
+        revalidateOnFocus: false,
+      }
+    );
+    useEffect(() => {
+      ethereum?.on("accountsChanged", handleAccountsChanged);
+      return () => {
+        ethereum?.removeListener("accountsChanged", handleAccountsChanged);
+      };
+    });
 
-  return swrRes;
-};
+    const handleAccountsChanged = (...args: unknown[]) => {
+      const accounts = args[0] as string[];
+      if (accounts.length === 0) {
+        console.error("Please, connect to Web3 wallet");
+      } else if (accounts[0] !== String(swrRes.data)) {
+        swrRes.mutate(accounts[0])
+      }
+    };
+
+    const connect = async () => {
+      try {
+        ethereum?.request({ method: "eth_requestAccounts" });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    return {
+      ...swrRes,
+      connect,
+    };
+  };
--- types/hooks.ts
+++ types/hooks.ts
@@ -8,15 +8,15 @@ export type Web3Dependencies = {
   ethereum: MetaMaskInpageProvider;
 };
 
-export type CryptoHookFactory<D = any, P = any> = {
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>;
+export type CryptoHookFactory<D = any, R = any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, R, P>;
 };
 
-export type CryptoSWRResponse<D = any> = SWRResponse<D>;
+export type CryptoHandlerHook<D = any, R = any, P = any> = (
+  params?: P
+) => CryptoSWRResponse<D, R>;
 
-export type CryptoHandlerHook<D = any, P = any> = (
-  params: string
-) => CryptoSWRResponse<D>;
+export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R;
 
 // export type CryptoHookFactory<D = any, P = any> = {
 //   (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>;
